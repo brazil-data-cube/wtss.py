baf28948610c6c59203ecf271c464fb64e25c5cc
gqueiroz <gribeiro.mg@gmail.com>
2020-09-05 20:50:10 -0300

    Improve repository documentation and organization (ref #1, #2, #3, #4; closes #6)

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 #
 # This file is part of Python Client Library for WTSS.
-# Copyright (C) 2019 INPE.
+# Copyright (C) 2020 INPE.
 #
 # Python Client Library for WTSS is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
-pydocstyle wtss && \
-isort --check-only --diff --recursive **/*.py && \
+pydocstyle wtss tests setup.py && \
+isort wtss tests setup.py --check-only --diff && \
 check-manifest --ignore ".travis-*" --ignore ".readthedocs.*" && \
-pytest && \
-sphinx-build -qnW --color -b doctest doc/sphinx/ doc/sphinx/_build/doctest
+sphinx-build -qnW --color -b doctest docs/sphinx/ docs/sphinx/_build/doctest && \
+pytest
+
--- wtss/__init__.py
+++ wtss/__init__.py
@@ -1,6 +1,6 @@
 #
 # This file is part of Python Client Library for WTSS.
-# Copyright (C) 2019 INPE.
+# Copyright (C) 2020 INPE.
 #
 # Python Client Library for WTSS is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.

413db16adbcea5b018dc5b11846094c7704784a4
gqueiroz <gribeiro.mg@gmail.com>
2021-01-15 15:18:30 -0300

    First version of Drone support (closes #54)

--- run-tests.sh
+++ run-tests.sh
@@ -9,6 +9,6 @@
 
 pydocstyle wtss examples tests setup.py && \
 isort wtss examples tests setup.py --check-only --diff && \
-check-manifest --ignore ".travis-*" --ignore ".readthedocs.*" && \
+check-manifest --ignore ".drone.*" --ignore ".readthedocs.*" && \
 sphinx-build -qnW --color -b doctest docs/sphinx/ docs/sphinx/_build/doctest && \
 pytest
